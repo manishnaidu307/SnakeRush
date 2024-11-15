edb98e10948a8b4d637303e091deaa3a064b9b54
Manish Naidu <138713291+manishnaidu307@users.noreply.github.com>
2024-11-15 11:27:42 +0530

    Update gameFrame.java

--- Game FIles/gameFrame.java
+++ Game FIles/gameFrame.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 public class gameFrame extends JFrame{
     gameFrame(){
         this.add(new gamePanel());
-        this.setTitle("Snake");
+        this.setTitle("SnakeRush");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
